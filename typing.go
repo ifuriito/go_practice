09b7e296d77816d7d17da7aea680f4c753c5d180
ifuriito <y.ota.rkt@gmail.com>
2020-07-19 03:09:32 +0900

    関数にまとめる

--- typing.go
+++ typing.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var input string
-	fmt.Println("次の単語を入力してください: test");
-	fmt.Scan(&input);
-	fmt.Printf("%sと入力されました",input);
+	ask()
+}
+
+func ask() {
+    var input string
+    fmt.Println("次の単語を入力してください: test")
+    fmt.Scan(&input)
+    fmt.Printf("%sと入力されました", input)
 }
