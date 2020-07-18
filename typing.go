7e4d7455bcd2ca6864526d1642488636f1d88599
ifuriito <y.ota.rkt@gmail.com>
2020-07-19 04:00:28 +0900

    スコアの原型の追加

--- typing.go
+++ typing.go
@@ -3,17 +3,16 @@ package main
 import "fmt"
 
 func main() {
-
-	ask(1, "hoge")
-
-	ask(2, "hogehoge")
-
-	ask(3, "hogehogehgoe")
+	// totalScoreにaskの戻り値を代入
+	totalScore := ask(1, "hoge")
+	fmt.Println("スコア", totalScore)
 }
 
-func ask(number int, question string) {
+// 戻り値の型を定義
+func ask(number int, question string) int{
 	var input string
 	fmt.Printf("[質問%d] 次の単語を入力してください: %s\n", number, question)
 	fmt.Scan(&input)
 	fmt.Printf("%sと入力されました\n", input)
+	return 10
 }
