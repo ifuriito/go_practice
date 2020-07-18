43d9001617bbea5211d0f54d032795ca2a45f8ba
ifuriito <y.ota.rkt@gmail.com>
2020-07-19 03:39:58 +0900

    問題番号の追加

--- typing.go
+++ typing.go
@@ -3,12 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	ask()
+
+	ask(1, "hoge")
+
+	ask(2, "hogehoge")
+
+	ask(3, "hogehogehgoe")
 }
 
-func ask() {
-    var input string
-    fmt.Println("次の単語を入力してください: test")
-    fmt.Scan(&input)
-    fmt.Printf("%sと入力されました", input)
+func ask(number int, question string) {
+	var input string
+	fmt.Printf("[質問%d] 次の単語を入力してください: %s\n", number, question)
+	fmt.Scan(&input)
+	fmt.Printf("%sと入力されました\n", input)
 }
