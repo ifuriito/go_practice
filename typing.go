e21f4ea568a32acb4880b72af1d9df2c0cde316d
ifuriito <y.ota.rkt@gmail.com>
2020-07-19 14:02:02 +0900

    ポインタを利用して、ask関数内にスコア計算の処理を入れる

--- typing.go
+++ typing.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	// totalScoreにaskの戻り値を代入
-	totalScore := ask(1, "hoge")
-	totalScore += ask(2, "hogehoge")
-	totalScore += ask(3, "hogehoge")
+	totalScore := 0
+	ask(1, "hoge", &totalScore)
+	ask(2, "hogehoge", &totalScore)
+	ask(3, "hogehogehoge", &totalScore)
 	fmt.Println("スコア", totalScore)
 }
 
-// 戻り値の型を定義
-func ask(number int, question string) int{
+// スコアの計算もポインタを使って、ask関数内で行う
+func ask(number int, question string, scorePtr *int) {
 	var input string
 	fmt.Printf("[質問%d] 次の単語を入力してください: %s\n", number, question)
 	fmt.Scan(&input)
@@ -19,10 +19,9 @@ func ask(number int, question string) int{
 
 	// 正解、不正解の判定をスコアに反映
 	if question == input {
-        fmt.Println("正解!")
-        return 10
+		fmt.Println("正解!")
+		*scorePtr += 10
 	} else {
         fmt.Println("不正解!")
-        return 0
 	}
 }
